8741d8149e2c9c94ae9972d14249a6f68d169c88
MVMAM <ediconpts9@gmail.com>
2023-11-24 17:07:38 +0500

    Auth NOT fully and with bugs in the static. JavaScript also has a bugs

--- PerfectChoice/main/urls.py
+++ PerfectChoice/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import *
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
+    path('login/', LoginView.as_view(template_name='main/allAuth/login.html'), name='login'),
 ]
