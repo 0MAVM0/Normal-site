02502978f81dc320b1cbce70fbe0a7de4654e809
MVMAM <ediconpts9@gmail.com>
2023-11-26 22:28:19 +0500

    Template 'home' NOT fully and JS not working again

---
+++ PerfectChoice/static/js/navbar.js
@@ -0,0 +1,34 @@
+const showMenu = (toggleId, navbarId, bodyId) => {
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypd = document.getElementById(bodyId)
+
+    if (toggle && navbar) {
+        toggle.addEventListener('click', () => {
+            navbar.classList.toggle('expander');
+
+            bodypadding.classList.toggle('body-pd')
+        })
+    }
+}
+showMenu('nav-toggle', 'navbar', 'body-pd')
+const linkColor = document.querySelectorAll('.nav__link');
+function colorLink() {
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+const linkCollapse = document.getElementsByClassName('collapse__link');
+
+var i;
+
+for (i = 0; i < linkCollapse.length; i++) {
+    linkCollapse[i].addEventListener('click', function() {
+        const collapseMenu = this.nextElementSibling;
+        collapseMenu.classList.toggle('showCollapse');
+
+        const rotate = collapseMenu.previoseElementSibling;
+        rotate.classList.toggle('rotate');
+    })
+}
