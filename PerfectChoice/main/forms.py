807645dfa019ae1bb064a4762e5644e0df0f3367
MVMAM <ediconpts9@gmail.com>
2023-12-06 12:10:18 +0500

    Work for Profile page & Migrations

--- PerfectChoice/main/forms.py
+++ PerfectChoice/main/forms.py
@@ -2,8 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Profile
+
 
 class UserForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'email'})
+    )
     username = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'User name', 'class': 'input'})
     )
@@ -18,4 +24,19 @@ class UserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserUpdateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'username-field'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-field'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-confirm-field'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('image',)
--- PerfectChoice/main/models.py
+++ PerfectChoice/main/models.py
@@ -7,3 +7,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
