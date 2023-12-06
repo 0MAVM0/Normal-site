ddde7b514d90d895e42bad3fcb27ad5ec4e4cbb4
MVMAM <ediconpts9@gmail.com>
2023-12-06 12:42:01 +0500

    bug like ','

--- PerfectChoice/PerfectChoice/settings.py
+++ PerfectChoice/PerfectChoice/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Libraries
-    'django_bootstrap5'
+    'django_bootstrap5',
     #           DRF
     'djoser',
     'rest_framework',
