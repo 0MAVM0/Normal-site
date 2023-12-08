5103cff86b01e52a68bb0a7cf4c98ae187136b18
MVMAM <ediconpts9@gmail.com>
2023-12-08 18:15:18 +0500

    Changes in templates

--- PerfectChoice/Products/models.py
+++ PerfectChoice/Products/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -11,3 +12,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 600 or img.width > 600:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
