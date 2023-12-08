ee260202ad9f8d48262a8943102d36cca71ffae6
MVMAM <ediconpts9@gmail.com>
2023-12-08 17:58:54 +0500

    Nothing

--- PerfectChoice/Products/models.py
+++ PerfectChoice/Products/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    image = models.ImageField( upload_to='img/')
+    image = models.ImageField(upload_to='img/')
     description = models.TextField()
     price = models.IntegerField()
 
