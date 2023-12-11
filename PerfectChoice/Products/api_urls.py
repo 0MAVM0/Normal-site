5bc9f1073ecc6e223e1bd7a4a40f70609436dea1
MVMAM <ediconpts9@gmail.com>
2023-12-11 14:54:16 +0500

    About & Logout & Migrations

--- PerfectChoice/Products/api_urls.py
+++ PerfectChoice/Products/api_urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+
 from .api_views import ProductViewSet
+
 
 urlpatterns = [
     path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name='product-list'),
     path(
         '<int:pk>/',
-        ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
+        ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy_product'}),
         name='product-detail'
     ),
 ]
--- PerfectChoice/Products/api_views.py
+++ PerfectChoice/Products/api_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 
 from .models import Product
@@ -50,3 +50,9 @@ class ProductViewSet(AuthApiView, viewsets.ModelViewSet):
         elif request.method == 'DELETE':
             product.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['DELETE'])
+    def destroy_product(self, request, pk=None):
+        product = self.get_object()
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
